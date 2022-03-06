70a304f8e7fc6eda6d1aa9700bd2c93267f5b502
NataTyugun <99788525+NataTyugun@users.noreply.github.com>
2022-03-07 02:14:04 +0300

    Add files via upload

---
+++ ДЗ4  н 6.py
@@ -0,0 +1,10 @@
+import math
+def my_log(lst: list) -> list:
+    l = []
+    for i in len(lst):
+        if lst[i]>0 :
+            l[i] = math.log(lst[i])
+        else :
+            l[i] = None
+    return l
+
